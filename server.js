7f5066b0e65f202e2372902e492ddf45c5afbc20
Minh Le <duyminh.le310@gmail.com>
2016-07-07 20:41:05 +0000

    Final

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var multer = require('multer');
+
+var upload = multer({dest: 'upload/'});
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use("/", express.static("view"));
+
+app.post("/uploadInfo",upload.single("uploadFile"), function(req, res, next) {
+    res.json({
+      size: req.file.size});
+    
+    res.end();
 });
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
